7b2d42f94f29c723a2d9b4096e45b3b6def328e9
BRajapandi <rajapandib29@gmail.com>
2024-02-23 13:15:10 +0530

    about page first col setup

--- src/Component/about/About.js
+++ src/Component/about/About.js
@@ -1,37 +1,59 @@
 import Wrapped from "./AboutStyle";
-import about_banner from "../../asserts/images/banner3.png";
+import AboutImg from "../../asserts/svg/aboutImgBack.svg";
+import Profile from "../../asserts/mine/profile2.png";
 import { MdWorkHistory } from "react-icons/md";
 import { TbBrandReact } from "react-icons/tb";
+import CurvedArrow from "../../asserts/svg/curled-arrow.svg";
 import { TbBrandNodejs } from "react-icons/tb";
 import { BiLogoPostgresql } from "react-icons/bi";
 
 function About() {
   return (
-    <Wrapped>
-      <div className="bannerWrapper">
-        <img src={about_banner} className="bannerImg" />
-      </div>
-      <div className="primarySkillSize">
-        <div className="primarySkillsWrapper">
-          <div className="primarySkill">
-            <MdWorkHistory className="icon" />
-            <span>Experience</span>
+    <Wrapped text="blue">
+      <div className="AboutWrapper">
+        <div className="firstRow">
+          <div className="firstCol">
+            <div className="firstImgBGWrapper">
+              <img src={AboutImg} className="firstImgBG" />
+            </div>
+            <div className="firstMineImgWrapper">
+              <img src={Profile} className="firstMineImg" />
+            </div>
+            <div className="arrowWrapper">
+              <img src={CurvedArrow} />
+            </div>
+            <div className="cardalign firstColCard1">
+              <span className="icon">
+                <MdWorkHistory />
+              </span>
+              <span className="content">
+                <div>Total Experience</div>
+                <div>3 years 2 months</div>
+              </span>
+            </div>
+            <div className="cardalign firstColCard2">
+              <span className="icon">
+                <TbBrandReact />
+              </span>
+              <span className="content">
+                <div>Experience React js</div>
+                <div>3 years 2 months</div>
+              </span>
+            </div>
           </div>
-          <div className="primarySkill">
-            <TbBrandReact className="icon" />
-            <span>React js</span>
-          </div>
-          <div className="primarySkill">
-            <TbBrandNodejs className="icon" />
-            <span>Node & Express js</span>
-          </div>
-          <div className="primarySkill">
-            <BiLogoPostgresql className="icon" />
-            <span>Postgresql</span>
+          <div className="secondCol">
+            <div className="Title">ABOUT</div>
+            <div className="subTitle">Nice to Meet you all</div>
+            <div className="aboutme">
+              I'm Rajapandi from Madurai and I'm a full-stack developer with 3
+              years and 2 months of experience. I have completed a bachelor’s of
+              engineering with a specialization in computer science and
+              engineering in 2020. Currently, I'm working as a Software Engineer
+              at tarnea technology solution private limited.
+            </div>
           </div>
         </div>
       </div>
-      <div></div>
     </Wrapped>
   );
 }
--- src/Component/about/AboutStyle.js
+++ src/Component/about/AboutStyle.js
@@ -1,29 +1,132 @@
 import styled from "styled-components";
-
+import AboutBg from "../../asserts/svg/backgound.svg";
 const Wrapped = styled.div`
-  .bannerWrapper {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding-top: 3vh;
-    .bannerImg {
-      height: 300px;
-      width: 80vw;
+  padding-top: 3vh;
+  .AboutWrapper {
+    padding-top: calc(6vh - 15px);
+    min-height: 100vh;
+    .firstRow {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 1rem;
+      height: 80vh;
+      .firstCol {
+        position: relative;
+        height: 100%;
+        width: 100%;
+        .firstImgBGWrapper {
+          height: 100%;
+          width: 100%;
+          position: absolute;
+          top: 70%;
+          left: 50%;
+          transform: translate(-50%, -50%) rotate(0deg);
+        }
+        .firstMineImgWrapper {
+          width: 68%;
+          position: absolute;
+          top: 45%;
+          left: 53%;
+          transform: translate(-50%, -50%) rotate(0deg);
+          .firstMineImg {
+            filter: brightness(1.3) contrast(111%) saturate(1.1);
+          }
+        }
+        .cardalign {
+          background-color: var(--dark-text-color4);
+          display: grid;
+          grid-template-columns: 35px 1fr;
+          gap: 5px;
+          color: #fff;
+          padding: 2px 6px;
+          border-radius: 6px;
+          box-shadow: 1px 1px 3px var(--dark-text-color4);
+        }
+        .arrowWrapper {
+          position: absolute;
+          left: 5px;
+          top: 10px;
+          transform: rotateZ(348deg);
+        }
+        .firstColCard1 {
+          position: absolute;
+          left: 40px;
+          top: 180px;
+          .icon {
+            vertical-align: middle;
+            text-align: center;
+            color: var(--grey-900);
+            font-size: 28px;
+          }
+          .content {
+            color: var(--grey-900);
+            font-size: 16px;
+            font-weight: 600;
+            letter-spacing: 1cap.5px;
+          }
+        }
+        .firstColCard2 {
+          position: absolute;
+          right: 10px;
+          bottom: 10px;
+          .icon {
+            vertical-align: middle;
+            text-align: center;
+            color: var(--grey-900);
+            font-size: 28px;
+          }
+          .content {
+            color: var(--grey-900);
+            font-size: 16px;
+            font-weight: 600;
+            letter-spacing: 1cap.5px;
+          }
+        }
+      }
+      .secondCol {
+        padding-top: 100px;
+        color: #fff;
+        .Title {
+          font-size: 25px;
+          font-weight: 800;
+          letter-spacing: 2px;
+          /* position: relative; */
+          &::after {
+            /* position: absolute; */
+            content: "";
+            display: inline-block;
+            height: 0.1em;
+            vertical-align: middle;
+            width: 200px;
+            margin-right: -100%;
+            margin-left: 10px;
+            border-radius: 4px;
+            border-top: 4px solid var(--dark-text-color8);
+          }
+        }
+        .subTitle {
+          font-size: 20px;
+          font-weight: 600;
+          margin: 10px 0px;
+        }
+        .aboutme {
+          width: 70%;
+          margin: 15px 0px 0px 15px;
+          padding-left: 16px;
+          border-left: 2px dashed var(--dark-text-color8);
+          text-align: justify;
+          text-indent: 50px;
+          font-size: 20px;
+        }
+      }
     }
   }
-  .primarySkillSize {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    .primarySkillsWrapper {
-      width: 80vw;
-      display: grid;
-      align-items: center;
-      justify-content: center;
-      grid-template-columns: 1fr 1fr 1fr 1fr;
-      gap: 40px;
-      color: aliceblue;
-    }
+
+  .AboutWrapper {
+    background-image: url(${AboutBg});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
   }
 `;
 
